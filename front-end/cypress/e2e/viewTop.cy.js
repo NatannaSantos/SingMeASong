4219c3139dd03dfbeab073cff0a23ec29f43d5f3
Natanna <natannacortez@gmail.com>
2022-08-01 14:25:55 -0300

    fix:top page

--- front-end/cypress/e2e/viewTop.cy.js
+++ front-end/cypress/e2e/viewTop.cy.js
@@ -1,7 +1,13 @@
-describe('Navigate to page Top', () => {
+describe('Navigate to page Top', () => {    
 	it('should see all the videos that are at the top', () => {
 		cy.visit('http://localhost:3000');
 		cy.contains('Top').click();
 		cy.url().should('eq', 'http://localhost:3000/top');
     });
+    it('should be in descending order of upvotes', () => {
+		
+		cy.get('article:first div:last').should('have.text', '10');
+		cy.get('article:last div:last').should('have.text', '0');
+
+	});
 });
