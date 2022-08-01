67ba6682817847d4d15226a213c692503317bf94
Natanna <natannacortez@gmail.com>
2022-08-01 14:37:08 -0300

    fix: top page upvotes

--- front-end/cypress/e2e/viewTop.cy.js
+++ front-end/cypress/e2e/viewTop.cy.js
@@ -6,7 +6,7 @@ describe('Navigate to page Top', () => {
     });
     it('should be in descending order of upvotes', () => {
 		
-		cy.get('article:first div:last').should('have.text', '10');
+		cy.get('article:first div:last').should('have.text', '1');
 		cy.get('article:last div:last').should('have.text', '0');
 
 	});
