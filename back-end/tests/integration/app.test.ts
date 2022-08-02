65f43716705559eacb274a3848fffa90c452401f
Natanna <natannacortez@gmail.com>
2022-08-02 19:34:24 -0300

    feat: teste de integração busca de recomendação por id

--- back-end/tests/integration/app.test.ts
+++ back-end/tests/integration/app.test.ts
@@ -1,8 +1,7 @@
-import {faker} from "@faker-js/faker";
 import supertest from "supertest";
 import app from "../../src/app.js";
 import { prisma } from "../../src/database.js";
-import { CreateRecommendationData, recommendationService } from "../../src/services/recommendationsService.js";
+import { recommendationService } from "../../src/services/recommendationsService.js";
 import recommendationBodyFactory from "../factories/recommendationBodyFactory.js";
 
 describe("performing integration tests",()=>{
@@ -41,5 +40,41 @@ describe("performing integration tests",()=>{
     })
     expect(response.status).toEqual(200);
     expect(createdRecommendation.score+1).toEqual(updatedRecommendation.score);
+    });
+
+    it("should persist the downvote of a recommendation given it exists", async ()=>{
+        const recommendation = recommendationBodyFactory();
+
+    const createdRecommendation = await prisma.recommendation.create({
+        data:recommendation
     })
+    const response = await supertest(app).post(`/recommendations/${createdRecommendation.id}/downvote`);
+    const updatedRecommendation = await prisma.recommendation.findUnique({
+        where:{
+            name:createdRecommendation.name
+        }
+    })
+    expect(response.status).toEqual(200);
+    expect(createdRecommendation.score-1).toEqual(updatedRecommendation.score);
+    });
+
+    it("should search for recommendations by id",async ()=>{
+        const recommendation = recommendationBodyFactory();
+
+        await prisma.recommendation.create({
+            data:recommendation
+        })
+
+        const recommendations = await prisma.recommendation.findFirst({
+            where:{
+                name:recommendation.name
+            }
+        });
+
+        
+        const response = await supertest(app).get(`/recommendations/${recommendations.id}`);
+
+        expect(response.status).toEqual(200);
+        expect(response.body).toEqual(recommendations);
+    });
 })
